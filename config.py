5314d1edd0b7706a38efe6f146376e177bf7abfa
nltoosicks <87550604+nltoosicks@users.noreply.github.com>
2024-08-03 00:37:27 -0400

    Database security measures

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://yourusername:yourpassword@localhost/photoshare'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
