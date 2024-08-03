580e0b8b4da47def0581e3143d7c4a7a0e36a3da
nltoosicks <87550604+nltoosicks@users.noreply.github.com>
2024-08-03 01:13:46 -0400

    Misc Updates

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
