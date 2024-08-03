0df79ddd69f817a328452546ee9b34d32d8da9ba
nltoosicks <87550604+nltoosicks@users.noreply.github.com>
2024-08-03 01:29:37 -0400

    feat: Add Flask-Bootstrap4 to the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap4
 from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bootstrap = Bootstrap4()
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
     login_manager.login_view = 'auth.login'
 
     from app.main import bp as main_bp
